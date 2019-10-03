069e4097bdf3210aa13d9c33e575d5a42329eeda
haemin9014 <55457623+haemin9014@users.noreply.github.com>
2019-10-03 12:13:45 -0700

    erased helloworld, not using it

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,39 +1,3 @@
 <template>
-  <v-container fluid>
-    <v-row align="center">      
-       <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="Semester"
-          label="Semester"
-          outlined
-        ></v-select>
-      </v-col>
 
-            <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="Class"
-          label="Class"
-          outlined
-        ></v-select>
-      </v-col>
-
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="Level"
-          label="Level"
-          outlined
-        ></v-select>
-      </v-col>      
-    </v-row>
-  </v-container>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      Semester: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-      Class: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-      Level: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-    }),
-  }
-</script>
