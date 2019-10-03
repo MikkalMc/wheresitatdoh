eb393ca79ad4d305715454947cbd8b4622da9fe3
haemin9014 <55457623+haemin9014@users.noreply.github.com>
2019-10-03 10:30:38 -0700

    before fix

--- frontend/src/components/Filters.vue
+++ frontend/src/components/Filters.vue
@@ -1,54 +1,12 @@
 <template>
-  <v-container fluid>
-    <v-row align="center">
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="items"
-          label="Standard"
-        ></v-select>
-      </v-col>
-
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="items"
-          filled
-          label="Filled style"
-        ></v-select>
-      </v-col>
-
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="items"
-          label="Outlined style"
-          outlined
-        ></v-select>
-      </v-col>
-
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select
-          :items="items"
-          label="Solo field"
-          solo
-        ></v-select>
-      </v-col>
-    </v-row>
-  </v-container>
-<!--
   <div class="filter">
     <label for="autocomplete-dropdown"> Class </label>
     <dropdown id="autocomplete-dropdown" :options="classOptions" v-model="selectedClass"></dropdown>
   </div>
--->
-
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-    }),
-  }
-/*
+
   import Dropdown from '@/components/Dropdown'
   import AutocompleteDropdown from '@/components/AutocompleteDropdown'
 export default {
@@ -70,10 +28,10 @@ export default {
         }
     }
 }
-*/
+
 
 </script>
-<!--
+
 <style scoped>
 
 .filters {
@@ -88,5 +46,5 @@ export default {
 label {
   display: block;
 }
--->
+
 </style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -38,7 +38,7 @@
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data () =>{
     return {
       msg: 'Welcome to Your Vue.js App'
     }
@@ -47,19 +47,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
